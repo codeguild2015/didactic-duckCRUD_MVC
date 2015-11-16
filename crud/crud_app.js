de774562c5779798a7914d47fa566c5fdc97fc56
Jaydn C <Jaydnc601@gmail.com>
2015-11-16 07:17:07 -0800

    working with comments

--- crud/crud_app.js
+++ crud/crud_app.js
@@ -1,143 +1,185 @@
-// This is everything I've got from all groups put together, not compelted
+// An MVC implementation of webpage that taps the Google Sheets API
+// for interaction with a single, predefined spreadsheet.  Front end
+// is a basic Bootstrap website from template.
+//
+// Author: Cole Howard (sorta also Patrick, Jaydn and Connor)
+//
+
+;  // Defensive
 
 function Wrapper() {
+
     'use strict';
-    var model = {};
 
-    var Model = { //Back end, talks to data structure and passes info to controller to be sent to the view
-        getModel : function() { 
-     
-          $(document).ready(function() {
-            var url = "https://spreadsheets.google.com/";
-            var feedType = "feeds/cells/";
-            var key = "1sYkU_8raV14Bqm33dWcaksC3iMm73DH9OMsooxSMItM/";
-            var sheetID = "od6/";
-            var etc = "public/values?alt=json"
+    var Model = {
+        url: "https://spreadsheets.google.com/feeds/cells/" +
+              "1sYkU_8raV14Bqm33dWcaksC3iMm73DH9OMsooxSMItM" +
+              "/od6/public/values?alt=json",
+        currentState: {},
 
-            $.getJSON(url + feedType + key + sheetID + etc, function(data) {
-              //var model = {}; //originally an array, now an object.
-              var col_high = 0;
-
-              $.each(data.feed.entry, function(key, value) {
-                var cur_col = Number(value.gs$cell.col);  //Returns column value
-                if (cur_col > col_high) {
-                  col_high = cur_col; //reset the value as we iterate
-                  model[(cur_col).toString()] = {};
-                  }
-                  model[value.gs$cell.col][value.gs$cell.row] = value.gs$cell.$t;
+        getModel: function(callback) {
+            var self = this;
+            $.getJSON(this.url, function(data) {
+                var col_high = 0;
+                $.each(data.feed.entry, function(key, value) {
+                    var cur_col = Number(value.gs$cell.col);  //Returns column value
+                    if (cur_col > col_high) {
+                        col_high = cur_col; //reset the value as we iterate
+                        self.currentState[(cur_col).toString()] = {};
+                    }
+                    self.currentState[value.gs$cell.col][value.gs$cell.row] = value.gs$cell.$t;
                 }); // End .each
-                console.log("inside = ", model);
-              }); // End .getJSON
-              console.log("outer = ", model);
-          }); // End JQuery wrapper
+                callback();
+            });
+        },
+
+        appendLine: function(data) {
+            var to_post = {};
+            $.each(data, function(k, v) {
+                to_post[Model.currentState[k]["1"]] = v;
+            });
+            var address = "https://sheetsu.com/apis/0a299348";
+            $.post(address, to_post, Controller.fetchData);
+        },
+
+        nonFunc : function() {
+            // Does nothing (a dummy callback for the initialization)
+        }
+    };
+
+    var Controller = {
         
-        }, // End getModel()
+        fetchData : function() {
 
-    setModel : function(num_val, alpha_val, greet_val) { // input parameters
-
-        var data = {};
-        $.each(to_post, function(k, v) { 
-            data[model[k]["1"]] = v;
-      })
-      var address = "https://sheetsu.com/apis/0a299348";
-      $.post(address, data);
-    }
-};
-
-    var Controller = {        
-        getData : function() {
-            return Model.data;
+            Model.getModel(function() {
+                document.getElementById("addrow").disabled = false;
+                View.drawTable(Model.currentState); // Initializes app and view
+            });
         },
 
-        updateData : function(data, position) {
-            Model.set(data, position);// position ~= column 
-            return this.getData(); 
+        updateData : function(pos_str) {
+            
+            Model.setData(data, position);
+            Model.getModel();             
+            return Model.currentState; // Get's updated model, view feeds update, controller tells model, model updates, passes back to controller to pass to view for refreshed page
         },
 
-        eventHandler : function(input) {
-            if(input.target.className === 'submit') {
-                this.updateData(data, end_of_list);
-                
+        eventHandler : function(e) {
+            // If Add Button is clicked
+            if (e.target.className === "adder") {
+                $(e.target).prop("disabled",true);
+                View.displayAdd(); // To add a row 
+
+            } else if (e.target.className === "sub") {
+                var addList = [];
+                var input = $('.add_box').val();
+                var adds = input.split(',');
+                for (var i = 0; i < adds.length; i++) { 
+                    addList.push($.trim(adds[i]));
+                }
+                Model.appendLine(adds);
+                View.removeAdd();
+                View.displayList();
+            } else if (e.target.className === "update") {
+                return this.fetchData();
             }
         }
     };
 
+
     var View = {
-        var table = $('table')
-        var tabrow = document.getElementsByTagName('tr');
-        var url = "https://spreadsheets.google.com/feeds/cells/1sYkU_8raV14Bqm33dWcaksC3iMm73DH9OMsooxSMItM/od6/public/values?alt=json";
+        table: $('table'),
+        rowmax: 0,
+        colmax: 0,
+        addform: $('#addrowform'),
 
-        $.getJSON(url, function(data) {
-            $variable = data
-            forElements();
-        });
+        drawTable: function(data) {
+            this.rowmax = 0;
+            this.colmax = 0;  
+            $("tbody").remove();
+            this.getMax(data);
+            this.createTable(data);
+        },
 
-        forElements = function() {
-            for (var i = 0; i < $variable.feed.entry.length; i++){
-                var row = $variable.feed.entry[i].gs$cell.row;
-                var col = $variable.feed.entry[i].gs$cell.col;
-                var txt = $variable.feed.entry[i].gs$cell.$t;
-                createCell(row, col, txt)
-            }
-        };
 
-        createCell = function(row, col, txt) {
-            if (!tabrow[row-1]) {
-                var createrow = table[0].insertRow(-1); //insertRow could be (row-1) for exact index instead of last position
-                var cell = createrow.insertCell(-1);
-                cell.setAttribute("id", row + "-" + col);
-                cell.setAttribute("class", "no-select")
-                cell.innerHTML = txt;
-            } else {
-                var newcell = tabrow[row-1].insertCell(-1); //insertCell could be (col-1) for exact index instead of last position
-                newcell.innerHTML = txt;
-                newcell.setAttribute("id", row + "-" + col);
-                newcell.setAttribute("class", "no-select")
-            }
-        };
-
-        $(document).click(function( event ) {
-            if ($(event.target).is('input[value=Enter]')) {
-                alert('enter')
-            }
-            else if ($(event.target).is('input[value=Edit]')) {
-                editFunc()
-            }
-            else if ($(event.target).is('input[value=Delete]')) {
-                deleteFunc()
-            }
-        });
-
-        editFunc = function () {
-            $(document).click(function( event ) {
-                var placehold
-                if ($(event.target).is('TD') && event.target.className == 'no-select') {
-                    var placehold = event.target.firstChild.nodeValue
-                    event.target.innerHTML = '<input type="text" placeholder="'+ placehold + '">'
-                    event.target.className = 'select'
+        getMax: function(data) {
+            //cycles through the Model and saves number of rows and columns
+            for (var i = 0;i < Object.keys(data).length; i++){
+                if(Object.keys(data)[i] > this.colmax) {
+                    this.colmax = Number(Object.keys(data)[i]);
                 }
-                else if ($(event.target).is('TD') && event.target.className == 'select') {
-                    event.target.innerHTML = placehold
-                    event.target.className = 'no-select'
-                }
-            })
-        };
-
-        deleteFunc = function () { 
-            $(document).click(function( event ) {
-                if ($(event.target).is('TD')) {
-                    //console.log(event.target.innerHTML)
-                    if (event.target.className == "select") {
-                        event.target.className = "no-select";
-                    }
-                    else if (event.target.className == "no-select") {
-                        event.target.className = "select";
+            }
+            for (var key in data){
+                var values = data[key];
+                for (var j = 0; j < Object.keys(values).length; j++) {
+                    if(Number(Object.keys(values)[j]) > this.rowmax) {
+                        this.rowmax =  Number(Object.keys(values)[j]);
                     }
                 }
-            })
+            }
+        },
+
+        createTable: function(data) {
+            //creates a table with the dimensions of rowmax and colmax
+            for (var r = 1; r <= this.rowmax; r++) {
+                var tr = this.table[0].insertRow(-1);
+                for (var c = 1; c <= this.colmax; c++) {
+                    var cell = tr.insertCell(-1);
+                    cell.setAttribute("id", r + ',' + c);
+                    this.populateCell(data, r, c, cell);
+                }
+            }
+        },
+
+        populateCell: function(data, r, c, cell) {
+            //fills each cell with text, empty cells have placeholder text to maintain table shape
+            if (data[c] && data[c][r]) {
+                cell.innerHTML = data[c][r];
+            }
+            else {
+                cell.innerHTML = '&nbsp';
+            }   
+        },
+
+        addNewRow: function() {
+            //appends a row of text boxes to bottom of the table to pass to Controller
+            var tr = this.table[0].insertRow(this.rowmax);
+            for (var c = 1; c <= this.colmax; c++) {
+                var cell = tr.insertCell(-1);
+                cell.setAttribute("id", (c));
+                cell.innerHTML = '<input type="text">';
+            }
+        },
+
+        addDone: function() {
+            var sendData = {};
+            //creates a Submit button to send to Controller
+            var $done = $('<input type="submit" value="Done" id="donebutton" class="btn btn-default" />');
+            var text = $(":text");
+            $done.appendTo(this.addform); 
+            $(":submit").click(function() {
+                event.preventDefault();
+                for (var k = 0; k < $(":text").length; k++) {
+                    sendData[$(":text")[k].parentNode.getAttribute("id")] = $(":text")[k].value;
+                    $done.remove();
+                }
+            console.log(sendData);
+            Model.appendLine(sendData);
+            });                     
         }
-    }
+    };
 
-};
+    
+    Controller.fetchData();
+
+
+
+    $("#addrow").click(function() {
+        document.getElementById("addrow").disabled = true;
+        View.addNewRow();
+        View.addDone();
+    });
+
+}
 
 Wrapper();
